389f4f8399c0ea3154ec89dad0275de29627506a
BegnazarAkh <begnazaraxmadjonov@mail.ru>
2023-08-16 15:47:27 +0200

    Stage 0, Module 3, Task 5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        int num = toBeDivided / 5;
-        System.out.println(num);
+        double result = (double) toBeDivided / 5;
+        System.out.println("Result: " + result);
     }
 }
